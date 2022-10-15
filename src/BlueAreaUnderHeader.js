ece3e14715521b00d3dacf651646975331e0c08b
unknown <1996.a.n.d.r.e.y1996@gmail.com>
2022-10-15 14:36:39 +0300

    small changes

---
+++ src/BlueAreaUnderHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BlueAreaUnderHeader.css';
+
+function BlueAreaUnderHeader() {
+    return (
+      <div className="Blue-area-under-header">
+        
+      </div>
+    );
+  }
+  
+  export default BlueAreaUnderHeader;
