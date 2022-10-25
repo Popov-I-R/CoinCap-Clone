790ec7cc9403fe97b9393b1538df393faf04b8d0
Popov I R <farm03007@gmail.com>
2022-10-25 15:16:23 +0300

    Volume bug fixed

--- src/pages/AssetsPage/Asset.js
+++ src/pages/AssetsPage/Asset.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {useParams} from "react-router-dom";
+import BlueBarForDetailsOfCoin from './BlueBarForDetailsOfCoin/BlueBarForDetailsOfCoin';
 
 const AssetID = () => {
     const { assetIdentificator } = useParams()
-    console.log(assetIdentificator)
+    
     return( 
         <div> 
+        <BlueBarForDetailsOfCoin></BlueBarForDetailsOfCoin>
             <h4>{assetIdentificator}</h4>
         </div>
     )
--- src/pages/SwapPage/Swap.js
+++ src/pages/SwapPage/Swap.js
@@ -1,14 +1,13 @@
 import React from "react";
 import SwapComponent from "./SwapComponent";
 import "./Swap.css"
-import BlueBarForDetailsOfCoin from "../HomePage/BlueBarForDetailsOfCoin/BlueBarForDetailsOfCoin";
 
 export default function Swap() {
 
 
   return (
     <div>
-      <BlueBarForDetailsOfCoin />
+
       <div className="Swap-component-main-container">
         <SwapComponent />
       </div>
